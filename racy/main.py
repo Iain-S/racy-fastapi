a0e52bcfd47788af712270b7e30bc719293dc7b7
Iain <25081046+Iain-S@users.noreply.github.com>
2023-02-21 19:59:41 +0000

    Add other examples & pre-commit config

--- racy/main.py
+++ racy/main.py
@@ -3,11 +3,13 @@ from typing import Union
 import asyncio
 import databases
 import uvicorn
+from asyncpg.exceptions import LockNotAvailableError
 from fastapi import FastAPI
 from sqlalchemy.sql import text
 
 app = FastAPI()
 
+# Should match that in alembic.ini
 database = databases.Database("postgresql://postgres:password@localhost/postgres")
 
 
@@ -18,12 +20,71 @@ async def startup() -> None:
 
 @app.get("/")
 async def read_root():
+    """Will run fine if called synchronously but will error if called concurrently."""
     results = await database.execute(text("select count(*) from test_table"))
-    # results = [{**x} for x in await database.fetch_all(text("select * from test_table"))]
     await asyncio.sleep(5)
     await database.execute(text("insert into test_table values ({}, {})".format(results+1, 0)))
+    return {"hello": str(results)}
+
+
+@app.get("/show-table")
+async def read_root():
+    """Show all of the rows in test_table."""
+    results = [{**x} for x in await database.fetch_all(text("select * from test_table"))]
+    return results
+
+
+@app.get("/nowait")
+async def nowait():
+    """Using NOWAIT seems to stop the race condition but we need to handle the exception."""
+    async with database.connection() as connection:
+        async with connection.transaction() as t:
+            try:
+                results = await database.fetch_all(text("select id from test_table for update nowait"))
+
+                # It's fine to SELECT ... NOWAIT more than once in the same transaction
+                await database.fetch_all(text("select id from test_table for update nowait"))
+
+                await asyncio.sleep(3)
+                max_id = max([x["id"] for x in results])
+                await database.execute(text("insert into test_table values ({}, {})".format(max_id+1, 0)))
+            except LockNotAvailableError:
+                return "Table busy"
+    
+    return {"hello": str(max_id)}
+
+
+@app.get("/with-for-update")
+async def with_for_update():
+    """Using FOR UPDATE does not stop the race condition."""
+    async with database.connection() as connection:
+        async with connection.transaction() as t:
+            results = await database.fetch_all(text("select id from test_table for update"))
+            await asyncio.sleep(3)
+            max_id = max([x["id"] for x in results])
+            await database.execute(text("insert into test_table values ({}, {})".format(max_id+1, 0)))
+    
+    return {"hello": str(max_id)}
+
+
+@app.get("/explicit-lock")
+async def explicit_lock():
+    """Using LOCK seems to stop the race condition but we need to avoid deadlock."""
+    async with database.connection() as connection:
+        async with connection.transaction() as t:
+            await database.execute(text("lock table test_table"))
+            results = await database.execute(text("select count(*) from test_table"))
+            await asyncio.sleep(3)
+            await database.execute(text("insert into test_table values ({}, {})".format(results+1, 0)))
     
     return {"hello": str(results)}
+
+
+@app.get("/sync")
+def sync():
+    """Using a normal function does ..."""
+    # ToDo Can we use psycopg directly here?
+    raise NotImplementedError()
 
 
 @app.get("/items/{item_id}")
